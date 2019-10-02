e5f619ce2cb7e891167d36e394905093fe2736b9
Muhammad Usama Navid <muqadir1@gmail.com>
2019-10-03 00:45:48 +0500

    Create Remove spaces from a string.py

---
+++ Remove spaces from a string.py
@@ -0,0 +1,7 @@
+"""
+Given a stringa string as input, output it without spaces.
+For example
+String = 'ab c d  e fgh i jk  lmno  p qr stu v wxyz'
+Result = 'abcdefghijklmnopqrstuvwxyz'
+"""
+print(input().replace(" ",""))
